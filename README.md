c6e3f4bd276a235444aa2fda6461f09d965b8eeb
James Waples <jamwaffles@gmail.com>
2018-02-04 23:51:09 +0000

    Add example

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SSD1306 driver
 
 Currently only implemented for the 128x64 4-wire SPI protocol OLED module commonly found on eBay and the like.
+
+This is a huge WIP. The example just dumps a test pattern to the screen at time of writing, just to prove it's working. More to follow...
---
+++ examples/single_task.rs
@@ -0,0 +1,61 @@
+//! Send the test pattern to the OLED display connected to SPI1 on an STM32F103 "Blue Pill"
+//! This example does not use the `app!` macro from RTFM bust instead just starts a single task in main()
+
+#![no_std]
+
+extern crate cortex_m;
+extern crate cortex_m_semihosting as sh;
+extern crate stm32f103xx_hal as blue_pill;
+extern crate embedded_hal as hal;
+
+extern crate ssd1306;
+
+use blue_pill::prelude::*;
+use blue_pill::spi::{ Spi };
+use hal::spi::{ Mode, Phase, Polarity };
+use ssd1306::SSD1306;
+
+fn main() {
+    let dp = blue_pill::stm32f103xx::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    // SPI1
+    let sck = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
+    let miso = gpioa.pa6;
+    let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+
+    let rst = gpiob.pb0.into_push_pull_output(&mut gpiob.crl);
+    let dc = gpiob.pb1.into_push_pull_output(&mut gpiob.crl);
+
+    let spi = Spi::spi1(
+        dp.SPI1,
+        (sck, miso, mosi),
+        &mut afio.mapr,
+        Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnFirstTransition,
+        },
+        8.mhz(),
+        clocks,
+        &mut rcc.apb2,
+    );
+
+    let mut disp = SSD1306::new(spi, rst, dc);
+
+    disp.reset();
+
+    disp.init();
+
+    // TODO: Write some data
+
+    disp.flush();
+}

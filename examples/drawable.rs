395873c13a9fc35d21d8bdaf07e5214a75ea3b64
James Waples <jamwaffles@gmail.com>
2018-03-11 22:34:36 +0000

    Use primitives::new() methods
    Also RustFmt

--- examples/drawable.rs
+++ examples/drawable.rs
@@ -3,17 +3,17 @@
 #![no_std]
 
 extern crate cortex_m;
-extern crate stm32f103xx_hal as blue_pill;
 extern crate embedded_hal as hal;
+extern crate stm32f103xx_hal as blue_pill;
 
 extern crate ssd1306;
 
 use blue_pill::prelude::*;
-use blue_pill::spi::{ Spi };
-use hal::spi::{ Mode, Phase, Polarity };
+use blue_pill::spi::Spi;
+use hal::spi::{Mode, Phase, Polarity};
 
-use ssd1306::embedded_graphics::primitives;
-use ssd1306::{ SSD1306SPI, Drawing, Builder };
+use ssd1306::embedded_graphics::primitives::{Circle, Line, Rect};
+use ssd1306::{Builder, Drawing, SSD1306SPI};
 
 fn main() {
     let dp = blue_pill::stm32f103xx::Peripherals::take().unwrap();
@@ -51,17 +51,13 @@ fn main() {
 
     let mut disp = Builder::new().connect_spi(spi, rst, dc);
 
-    let tri1 = primitives::Line { start: (8, 16 + 16), end: (8 + 16, 16 + 16), color: 1 };
-    let tri2 = primitives::Line { start: (8, 16 + 16), end: (8 + 8, 16), color: 1 };
-    let tri3 = primitives::Line { start: (8 + 16, 16 + 16), end: (8 + 8, 16), color: 1 };
+    disp.draw(Line::new((8, 16 + 16), (8 + 16, 16 + 16), 1).pixels());
+    disp.draw(Line::new((8, 16 + 16), (8 + 8, 16), 1).pixels());
+    disp.draw(Line::new((8 + 16, 16 + 16), (8 + 8, 16), 1).pixels());
 
-    disp.draw(tri1.into_iter());
-    disp.draw(tri2.into_iter());
-    disp.draw(tri3.into_iter());
+    disp.draw(Rect::new((48, 16), (48 + 16, 16 + 16), 1u8).into_iter());
 
-    disp.draw(primitives::Rect { top_left: (48, 16), bottom_right: (48 + 16, 16 + 16), color: 1u8 }.into_iter());
-
-    disp.draw(primitives::Circle { center: (96, 16 + 8), radius: 8, color: 1u8 }.into_iter());
+    disp.draw(Circle::new((96, 16 + 8), 8, 1u8).into_iter());
 
     disp.flush();
 }

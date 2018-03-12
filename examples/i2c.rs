9e0e41a6c4d6f460debf4fecab359b231d63dbf2
James Waples <jamwaffles@gmail.com>
2018-03-12 19:55:51 +0000

    Format

--- examples/i2c.rs
+++ examples/i2c.rs
@@ -5,15 +5,15 @@
 #![no_std]
 
 extern crate cortex_m;
-extern crate stm32f103xx_hal as blue_pill;
 extern crate embedded_hal as hal;
+extern crate stm32f103xx_hal as blue_pill;
 
 extern crate ssd1306;
 
 use blue_pill::prelude::*;
-use blue_pill::i2c::{ I2c, Mode, DutyCycle };
+use blue_pill::i2c::{DutyCycle, I2c, Mode};
 
-use ssd1306::{ SSD1306I2C, Drawing, Builder };
+use ssd1306::{Builder, Drawing};
 
 fn main() {
     let dp = blue_pill::stm32f103xx::Peripherals::take().unwrap();
@@ -34,7 +34,10 @@ fn main() {
         dp.I2C1,
         (scl, sda),
         &mut afio.mapr,
-        Mode::Fast { frequency: 400_000, duty_cycle: DutyCycle::Ratio1to1 },
+        Mode::Fast {
+            frequency: 400_000,
+            duty_cycle: DutyCycle::Ratio1to1,
+        },
         clocks,
         &mut rcc.apb1,
     );
@@ -45,9 +48,9 @@ fn main() {
     disp.rect((0, 0), (127, 63), 1u8);
 
     // Draw a triangle
-    disp.line((8, 16 + 16), (8 + 16, 16 + 16), 1u8);    // Bottom
-    disp.line((8, 16 + 16), (8 + 8, 16), 1u8);          // Left
-    disp.line((8 + 16, 16 + 16), (8 + 8, 16), 1u8);     // Right
+    disp.line((8, 16 + 16), (8 + 16, 16 + 16), 1u8); // Bottom
+    disp.line((8, 16 + 16), (8 + 8, 16), 1u8); // Left
+    disp.line((8 + 16, 16 + 16), (8 + 8, 16), 1u8); // Right
 
     // Draw a square
     disp.rect((48, 16), (48 + 16, 16 + 16), 1u8);

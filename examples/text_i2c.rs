8540d9e34758a6cf5b67613333dde636c8670588
James Waples <jamwaffles@gmail.com>
2018-03-17 23:32:07 +0000

    Text drawing example

---
+++ examples/text_i2c.rs
@@ -0,0 +1,53 @@
+//! Draw a square, circle and triangle on the screen using I2C1 on pins B9 and B9 on the Blue Pill
+//! dev board. I found that it requires pullup resistors on SDA and SCL to function correctly
+
+#![no_std]
+
+extern crate cortex_m;
+extern crate embedded_graphics;
+extern crate embedded_hal as hal;
+extern crate ssd1306;
+extern crate stm32f103xx_hal as blue_pill;
+
+use blue_pill::i2c::{DutyCycle, I2c, Mode};
+use blue_pill::prelude::*;
+use embedded_graphics::Drawing;
+use ssd1306::Builder;
+use embedded_graphics::fonts::{Font, Font6x8};
+
+fn main() {
+    let dp = blue_pill::stm32f103xx::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    let i2c = I2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400_000,
+            duty_cycle: DutyCycle::Ratio1to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+    );
+
+    let mut disp = Builder::new().connect_i2c(i2c);
+    disp.init();
+    disp.flush();
+
+    disp.draw(Font6x8::render_str("Hello world!", (0, 0)).into_iter());
+    disp.draw(Font6x8::render_str("Hello Rust!", (0, 16)).into_iter());
+
+    disp.flush();
+}

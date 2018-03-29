d1d2f13ddbf9046c26ef415fbbc640e92b13480c
James Waples <james@cambridge-intelligence.com>
2018-03-29 15:09:02 +0100

    Build examples in CI, don't run tests
    We don't have any tests yet, it fails with an error.

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,16 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cross build --examples --target $TARGET
+    cross build --examples --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    # TODO: Uncomment when we have some tests
+    # cross test --target $TARGET
+    # cross test --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys

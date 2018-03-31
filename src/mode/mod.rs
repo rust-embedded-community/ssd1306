3f24947278bb9a516b8d7503819ac546bfe9a47b
Daniel Egger <daniel@eggers-club.de>
2018-03-31 21:53:43 +0200

    Rebased CharMode PoC code onto master and renamed to CharacterMode
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- src/mode/mod.rs
+++ src/mode/mod.rs
@@ -1,8 +1,10 @@
 //! Abstraction of different operating modes for the SSD1306
 
+pub mod character;
 pub mod displaymode;
 pub mod graphics;
 pub mod raw;
 
+pub use self::character::CharacterMode;
 pub use self::graphics::GraphicsMode;
 pub use self::raw::RawMode;

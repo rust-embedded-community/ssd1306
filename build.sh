90630d2b2774f828de36e16ba035f0f7aac9ba93
James Waples <jamwaffles@gmail.com>
2018-04-01 23:28:46 +0100

    Add doc upload to Travis build

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-set  -e
+set -e
 
-xargo build --target $TARGET --all-features
 xargo build --target $TARGET --all-features --release
 
 if [ -z $DISABLE_EXAMPLES ]; then
-	xargo build --target $TARGET --all-features --examples
 	xargo build --target $TARGET --all-features --examples
 fi

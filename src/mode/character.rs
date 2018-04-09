0efe2df86a325fb6899c8af74231c397edc0f878
Daniel Egger <daniel@eggers-club.de>
2018-04-09 23:31:37 +0200

    Create a new Bitmap trait and moved the font rendering out to implement it
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- src/mode/character.rs
+++ src/mode/character.rs
@@ -11,69 +11,18 @@ use mode::displaymode::DisplayModeTrait;
 
 use core::fmt;
 
-/// Handling structure for character mode display
-pub struct CharacterMode<DI> {
-    properties: DisplayProperties<DI>,
+/// A trait representing an a conversion to a 8x8 bitmap
+pub trait Bitmap<T> {
+    /// Turn input of type T into a displayable 8x8 bitmap
+    fn to_bitmap(input: T) -> [u8; 8];
 }
 
-impl<DI> DisplayModeTrait<DI> for CharacterMode<DI>
-where
-    DI: DisplayInterface,
-{
-    /// Create new CharacterMode instance
-    fn new(properties: DisplayProperties<DI>) -> Self {
-        CharacterMode { properties }
-    }
+/// The built-in default 7x7 font
+struct DefaultFont {}
 
-    /// Release all resources used by CharacterMode
-    fn release(self) -> DisplayProperties<DI> {
-        self.properties
-    }
-}
-
-impl<DI> CharacterMode<DI>
-where
-    DI: DisplayInterface,
-{
-    /// Clear the display buffer. You need to call `disp.flush()` for any effect on the screen
-    pub fn clear(&mut self) {
-        let display_size = self.properties.get_size();
-
-        let numchars = match display_size {
-            DisplaySize::Display128x64 => 128,
-            DisplaySize::Display128x32 => 64,
-            DisplaySize::Display96x16 => 24,
-        };
-
-        for _ in 0..numchars {
-            let _ = self.properties.draw(&[0; 8]);
-        }
-
-        // Reset position so we don't end up in some random place of our cleared screen
-        let _ = self.set_position(0, 0);
-    }
-
-    /// Reset display
-    pub fn reset<RST, DELAY>(&mut self, rst: &mut RST, delay: &mut DELAY)
-    where
-        RST: OutputPin,
-        DELAY: DelayMs<u8>,
-    {
-        rst.set_high();
-        delay.delay_ms(1);
-        rst.set_low();
-        delay.delay_ms(10);
-        rst.set_high();
-    }
-
-    /// Write out data to display
-    pub fn flush(&mut self) -> Result<(), ()> {
-        Ok(())
-    }
-
-    /// Print characters on the display with the embedded 7x7 font
-    pub fn print_chars(&mut self, bytes: &[u8]) -> Result<(), ()> {
-        // A 7x7 font shamelessly borrowed from https://github.com/techninja/MarioChron/
+/// A 7x7 font shamelessly borrowed from https://github.com/techninja/MarioChron/
+impl Bitmap<u8> for DefaultFont {
+    fn to_bitmap(input: u8) -> [u8; 8] {
         const FONT_7X7: [u8; 672] = [
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,// (space)
         0x00, 0x00, 0x5F, 0x00, 0x00, 0x00, 0x00,// !
@@ -173,18 +122,84 @@ where
         0x08, 0x1C, 0x2A, 0x08, 0x08, 0x00, 0x00 // <-
     ];
 
+        // Create an array with our byte data instruction and a blank column at the end
+        let mut data: [u8; 8] = [0; 8];
+
+        // Calculate our index into the character table above
+        let index = (input as usize - 0x20) * 7;
+
+        // Populate the middle of the array with the data from the character array at the right index
+        data[0..7].copy_from_slice(&FONT_7X7[index..index + 7]);
+
+        data
+    }
+}
+
+/// Handling structure for character mode display
+pub struct CharacterMode<DI> {
+    properties: DisplayProperties<DI>,
+}
+
+impl<DI> DisplayModeTrait<DI> for CharacterMode<DI>
+where
+    DI: DisplayInterface,
+{
+    /// Create new CharacterMode instance
+    fn new(properties: DisplayProperties<DI>) -> Self {
+        CharacterMode { properties }
+    }
+
+    /// Release all resources used by CharacterMode
+    fn release(self) -> DisplayProperties<DI> {
+        self.properties
+    }
+}
+
+impl<DI> CharacterMode<DI>
+where
+    DI: DisplayInterface,
+{
+    /// Clear the display buffer. You need to call `disp.flush()` for any effect on the screen
+    pub fn clear(&mut self) {
+        let display_size = self.properties.get_size();
+
+        let numchars = match display_size {
+            DisplaySize::Display128x64 => 128,
+            DisplaySize::Display128x32 => 64,
+            DisplaySize::Display96x16 => 24,
+        };
+
+        for _ in 0..numchars {
+            let _ = self.properties.draw(&[0; 8]);
+        }
+
+        // Reset position so we don't end up in some random place of our cleared screen
+        let _ = self.set_position(0, 0);
+    }
+
+    /// Reset display
+    pub fn reset<RST, DELAY>(&mut self, rst: &mut RST, delay: &mut DELAY)
+    where
+        RST: OutputPin,
+        DELAY: DelayMs<u8>,
+    {
+        rst.set_high();
+        delay.delay_ms(1);
+        rst.set_low();
+        delay.delay_ms(10);
+        rst.set_high();
+    }
+
+    /// Write out data to display
+    pub fn flush(&mut self) -> Result<(), ()> {
+        Ok(())
+    }
+
+    /// Print characters on the display with the embedded 7x7 font
+    pub fn print_chars(&mut self, bytes: &[u8]) -> Result<(), ()> {
         for c in bytes {
-            // Create an array with our byte data instruction and a blank column at the end
-            let mut data: [u8; 8] = [0; 8];
-
-            // Calculate our index into the character table above
-            let index = (*c as usize - 0x20) * 7;
-
-            // Populate the middle of the array with the data from the character array at the right index
-            data[0..7].copy_from_slice(&FONT_7X7[index..index + 7]);
-
             // Send the pixel data to the display
-            self.properties.draw(&data)?
+            self.properties.draw(&DefaultFont::to_bitmap(*c))?
         }
 
         Ok(())

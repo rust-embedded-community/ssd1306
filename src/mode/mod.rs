87a6c8d9aa3a9193ab43d5891b414b2d361b6eed
James Waples <jamwaffles@gmail.com>
2018-04-17 12:39:29 +0100

    Mode module docs

--- src/mode/mod.rs
+++ src/mode/mod.rs
@@ -1,10 +1,13 @@
-//! Abstraction of different operating modes for the SSD1306
+//! Operating modes for the SSD1306
+//!
+//! This driver can be used in different modes. A mode defines how the driver will behave, and what
+//! methods it exposes. Look at the modes below for more information on what they expose.
 
-pub mod terminal;
 pub mod displaymode;
 pub mod graphics;
 pub mod raw;
+pub mod terminal;
 
-pub use self::terminal::TerminalMode;
 pub use self::graphics::GraphicsMode;
 pub use self::raw::RawMode;
+pub use self::terminal::TerminalMode;

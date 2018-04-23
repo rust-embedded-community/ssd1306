f19a704d5bed1bf070eeae32f8e9dc733a7d6e90
James Waples <jamwaffles@gmail.com>
2018-04-23 21:36:07 +0100

    Landing page examples
    Rather a lot of comment text, but this file is quite thin anyway, and they should get compiled when `cargo test` starts working, which means they shouldn't get _too_ stale.

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,122 @@
 //!
 //! It's possible to customise the driver to suit your display/application. Take a look at the
 //! [Builder] for available options.
+//!
+//! # Examples
+//!
+//! Examples can be found in
+//! [the examples/ folder](https://github.com/jamwaffles/ssd1306/blob/master/examples)
+//!
+//! ## Write text to the display without a framebuffer
+//!
+//! Uses [mode::TerminalMode].
+//!
+//! ```rust,no-run
+//! #![no_std]
+//!
+//! extern crate cortex_m;
+//! extern crate embedded_hal as hal;
+//! extern crate panic_abort;
+//! extern crate ssd1306;
+//! extern crate stm32f103xx_hal as blue_pill;
+//!
+//! use blue_pill::i2c::{DutyCycle, I2c, Mode};
+//! use blue_pill::prelude::*;
+//! use core::fmt::Write;
+//! use ssd1306::{mode::TerminalMode, Builder};
+//!
+//! fn main() {
+//!     let dp = blue_pill::stm32f103xx::Peripherals::take().unwrap();
+//!     let mut flash = dp.FLASH.constrain();
+//!     let mut rcc = dp.RCC.constrain();
+//!     let clocks = rcc.cfgr.freeze(&mut flash.acr);
+//!     let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+//!     let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+//!     let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+//!     let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+//!     let i2c = I2c::i2c1(
+//!         dp.I2C1,
+//!         (scl, sda),
+//!         &mut afio.mapr,
+//!         Mode::Fast {
+//!             frequency: 400_000,
+//!             duty_cycle: DutyCycle::Ratio1to1,
+//!         },
+//!         clocks,
+//!         &mut rcc.apb1,
+//!     );
+//!
+//!     let mut disp: TerminalMode<_> = Builder::new().connect_i2c(i2c).into();
+//!     disp.init().unwrap();
+//!     disp.clear().unwrap();
+//!
+//!     // Endless loop
+//!     loop {
+//!         for c in 97..123 {
+//!             let _ = disp.write_str(unsafe { core::str::from_utf8_unchecked(&[c]) });
+//!         }
+//!         for c in 65..91 {
+//!             let _ = disp.write_str(unsafe { core::str::from_utf8_unchecked(&[c]) });
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! ## Draw some text to the display
+//!
+//! Uses [mode::GraphicsMode] and [embedded_graphics](../embedded_graphics/index.html).
+//!
+//! ```rust,no-run
+//! #![no_std]
+//!
+//! extern crate cortex_m;
+//! extern crate embedded_graphics;
+//! extern crate embedded_hal as hal;
+//! extern crate panic_abort;
+//! extern crate ssd1306;
+//! extern crate stm32f103xx_hal as blue_pill;
+//!
+//! use blue_pill::i2c::{DutyCycle, I2c, Mode};
+//! use blue_pill::prelude::*;
+//! use embedded_graphics::fonts::Font6x8;
+//! use embedded_graphics::prelude::*;
+//! use ssd1306::{mode::GraphicsMode, Builder};
+//!
+//! fn main() {
+//!     let dp = blue_pill::stm32f103xx::Peripherals::take().unwrap();
+//!     let mut flash = dp.FLASH.constrain();
+//!     let mut rcc = dp.RCC.constrain();
+//!     let clocks = rcc.cfgr.freeze(&mut flash.acr);
+//!     let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+//!     let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+//!     let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+//!     let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+//!
+//!     let i2c = I2c::i2c1(
+//!         dp.I2C1,
+//!         (scl, sda),
+//!         &mut afio.mapr,
+//!         Mode::Fast {
+//!             frequency: 400_000,
+//!             duty_cycle: DutyCycle::Ratio1to1,
+//!         },
+//!         clocks,
+//!         &mut rcc.apb1,
+//!     );
+//!
+//!     let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+//!
+//!     disp.init().unwrap();
+//!     disp.flush().unwrap();
+//!     disp.draw(Font6x8::render_str("Hello world!").into_iter());
+//!     disp.draw(
+//!         Font6x8::render_str("Hello Rust!")
+//!             .translate((0, 16))
+//!             .into_iter(),
+//!     );
+//!     disp.flush().unwrap();
+//! }
+//! ```
 
 #![no_std]
 // TODO: Docs

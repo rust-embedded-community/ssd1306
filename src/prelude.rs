d92cf899a6bbe85ef77f2a24bfb07923579f469d
James Waples <jamwaffles@gmail.com>
2018-04-30 09:36:20 +0100

    Add prelude

---
+++ src/prelude.rs
@@ -0,0 +1,6 @@
+//! Crate prelude
+
+pub use super::displayrotation::DisplayRotation;
+pub use super::displaysize::DisplaySize;
+pub use super::interface::{I2cInterface, SpiInterface};
+pub use super::mode::{GraphicsMode, TerminalMode};

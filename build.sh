2c2e4365914096dac67fde80959220b83dd0c549
James Waples <jamwaffles@gmail.com>
2019-07-21 19:53:36 +0100

    Run lib/doc tests in CI

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -e
 
 cargo build --target $TARGET --all-features --release
 
+cargo test --lib --target x86_64-unknown-linux-gnu
+cargo test --doc --target x86_64-unknown-linux-gnu
+
 if [ -z $DISABLE_EXAMPLES ]; then
 	cargo build --target $TARGET --all-features --examples
 fi

fb15f600cf3b5412eaa26a95307cb6ff3a1b15d7
James Waples <james@wapl.es>
2019-12-17 20:45:20 +0000

    Set COM pin config correctly
    The display I have at least uses an alternate row connection mapping as
    opposed to sequential. Without this change, the display skips every
    second row and makes everything stretched 2x in Y

--- src/properties.rs
+++ src/properties.rs
@@ -61,7 +61,7 @@ where
             DisplaySize::Display128x32 => Command::ComPinConfig(false, false).send(&mut self.iface),
             DisplaySize::Display128x64 => Command::ComPinConfig(true, false).send(&mut self.iface),
             DisplaySize::Display96x16 => Command::ComPinConfig(false, false).send(&mut self.iface),
-            DisplaySize::Display72x40 => Command::ComPinConfig(false, false).send(&mut self.iface),
+            DisplaySize::Display72x40 => Command::ComPinConfig(true, false).send(&mut self.iface),
         }?;
 
         Command::Contrast(0x8F).send(&mut self.iface)?;

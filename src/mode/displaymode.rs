b0754c54ec7db569d4372bf2b1148832f326429f
Dániel Buga <bugadani@gmail.com>
2020-06-08 10:43:01 +0200

    Remove rebase fail

--- src/mode/displaymode.rs
+++ src/mode/displaymode.rs
@@ -15,25 +15,3 @@ pub trait DisplayModeTrait<DI>: Sized {
         self.into_properties().release()
     }
 }
-
-impl<MODE> DisplayMode<MODE> {
-    /// Setup display to run in requested mode
-    pub fn new<DI>(properties: DisplayProperties<DI>) -> Self
-    where
-        DI: WriteOnlyDataCommand,
-        MODE: DisplayModeTrait<DI>,
-    {
-        DisplayMode(MODE::new(properties))
-    }
-
-    /// Change into any mode implementing DisplayModeTrait
-    // TODO: Figure out how to stay as generic DisplayMode but act as particular mode
-    pub fn into<DI, NMODE: DisplayModeTrait<DI>>(self) -> NMODE
-    where
-        DI: WriteOnlyDataCommand,
-        MODE: DisplayModeTrait<DI>,
-    {
-        let properties = self.0.into_properties();
-        NMODE::new(properties)
-    }
-}

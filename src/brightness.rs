9b2444ef9232feaa79f50417d2393cffec873c5e
Dániel Buga <bugadani@gmail.com>
2020-06-09 10:13:15 +0200

    Implement Default and use it when initializing

--- src/brightness.rs
+++ src/brightness.rs
@@ -7,6 +7,12 @@ pub struct Brightness {
     pub(crate) contrast: u8,
 }
 
+impl Default for Brightness {
+    fn default() -> Self {
+        Brightness::NORMAL
+    }
+}
+
 impl Brightness {
     /// The dimmest predefined brightness level
     pub const DIMMEST: Brightness   = Brightness::custom(0x1, 0x00);
@@ -25,11 +31,11 @@ impl Brightness {
 
     /// Create a Brightness object from a precharge period and contrast pair.
     ///
-    /// `precharge` sets the `phase 1` argument of the `0xD9 Set Pre-Charge Period` command and must 
-    /// be must be between 1 and 15. See section 10.1.17 of the SSD1306 datasheet for more 
+    /// `precharge` sets the `phase 1` argument of the `0xD9 Set Pre-Charge Period` command and must
+    /// be must be between 1 and 15. See section 10.1.17 of the SSD1306 datasheet for more
     /// information.
     ///
-    /// `contrast` sets the value used in the `0x81 Set Contrast Control` command and must be 
+    /// `contrast` sets the value used in the `0x81 Set Contrast Control` command and must be
     /// between 0 and 255. See section 10.1.7 of the SSD1306 datasheet for more information.
     const fn custom(precharge: u8, contrast: u8) -> Self {
         Self {

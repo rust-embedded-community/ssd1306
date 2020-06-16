f988370334789ace8a9a55d66f95d28883170566
Dániel Buga <bugadani@gmail.com>
2020-06-16 14:23:22 +0200

    No glob reexport

--- src/prelude.rs
+++ src/prelude.rs
@@ -7,6 +7,8 @@ pub use display_interface_spi::{SPIInterface, SPIInterfaceNoCS};
 pub use super::{
     brightness::Brightness,
     displayrotation::DisplayRotation,
-    displaysize::*,
+    displaysize::{
+        DisplaySize128x32, DisplaySize128x64, DisplaySize64x48, DisplaySize72x40, DisplaySize96x16,
+    },
     mode::{displaymode::DisplayModeTrait, GraphicsMode, TerminalMode},
 };

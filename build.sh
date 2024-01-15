9f538d2139a23fd5fe660242da1c21229dd4a8a5
Ivan Radeljak (irii) <ivan.radeljak@hotmail.de>
2024-01-15 18:52:34 +0100

    Add build support for other host architectures

--- build.sh
+++ build.sh
@@ -12,12 +12,18 @@ if [ -z $TARGET ]; then
     exit 1
 fi
 
+if [ -z $HOST_TARGET ]; then
+    echo "HOST_TARGET environment variable is not set - Falling back to default"
+fi
+HOST_TARGET=${HOST_TARGET:-x86_64-unknown-linux-gnu}
+
+
 cargo fmt --all -- --check
 
 cargo build --target $TARGET --all-features --release
 
-cargo test --lib --target x86_64-unknown-linux-gnu
-cargo test --doc --target x86_64-unknown-linux-gnu
+cargo test --lib --target $HOST_TARGET
+cargo test --doc --target $HOST_TARGET
 
 if [ -z $DISABLE_EXAMPLES ]; then
     cargo build --target $TARGET --all-features --examples

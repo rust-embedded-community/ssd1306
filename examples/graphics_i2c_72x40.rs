7c7637919ad8e50dbdd379274042a21101b3bd4a
Sjoerd Simons <sjoerd@collabora.com>
2024-06-22 23:36:05 +0200

    examples: Switch to embassy-stm32 as a pac provider
    stmf32f1xx doesn't yet support embedded-hal 1.0. Switch to embassy-stm32
    as a pac provider, which can be used in both a sync and async setups.
    
    Unfortunately for blocking i2c support embassy git has to be used rather
    then a releaed version. Also for i2c move the expected configuration
    from PB8/PB9 to PB6/PB7 to avoid the needed for pin remapping, which
    embassy doesn't yet support on stm32f1xx chips

--- examples/graphics_i2c_72x40.rs
+++ examples/graphics_i2c_72x40.rs
@@ -8,8 +8,8 @@
 //!      Display -> Blue Pill
 //! (black)  GND -> GND
 //! (red)    +5V -> VCC
-//! (yellow) SDA -> PB9
-//! (green)  SCL -> PB8
+//! (yellow) SDA -> PB7
+//! (green)  SCL -> PB6
 //! ```
 //!
 //! Run on a Blue Pill with `cargo run --example graphics_i2c`.
@@ -17,49 +17,27 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::{entry, exception, ExceptionFrame};
+use cortex_m::asm::nop;
+use cortex_m_rt::entry;
+use defmt_rtt as _;
+use embassy_stm32::time::Hertz;
 use embedded_graphics::{
     pixelcolor::BinaryColor,
     prelude::*,
     primitives::{Circle, PrimitiveStyleBuilder, Rectangle, Triangle},
 };
-use panic_halt as _;
+use panic_probe as _;
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
-use stm32f1xx_hal::{
-    i2c::{BlockingI2c, DutyCycle, Mode},
-    prelude::*,
-    stm32,
-};
 
 #[entry]
 fn main() -> ! {
-    let dp = stm32::Peripherals::take().unwrap();
-
-    let mut flash = dp.FLASH.constrain();
-    let rcc = dp.RCC.constrain();
-
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    let mut afio = dp.AFIO.constrain();
-
-    let mut gpiob = dp.GPIOB.split();
-
-    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
-    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
-
-    let i2c = BlockingI2c::i2c1(
-        dp.I2C1,
-        (scl, sda),
-        &mut afio.mapr,
-        Mode::Fast {
-            frequency: 400_000.Hz(),
-            duty_cycle: DutyCycle::Ratio2to1,
-        },
-        clocks,
-        1000,
-        10,
-        1000,
-        1000,
+    let p = embassy_stm32::init(Default::default());
+    let i2c = embassy_stm32::i2c::I2c::new_blocking(
+        p.I2C1,
+        p.PB6,
+        p.PB7,
+        Hertz::khz(400),
+        Default::default(),
     );
 
     let interface = I2CDisplayInterface::new(i2c);
@@ -117,10 +95,7 @@ fn main() -> ! {
 
     display.flush().unwrap();
 
-    loop {}
-}
-
-#[exception]
-unsafe fn HardFault(ef: &ExceptionFrame) -> ! {
-    panic!("{:#?}", ef);
+    loop {
+        nop()
+    }
 }
